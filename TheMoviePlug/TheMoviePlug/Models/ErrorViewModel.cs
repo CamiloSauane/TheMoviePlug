782cf1df9a97df7e1e9c477be90d18cda2d3f908
Camilo Sauane <62102997+CamiloSauane@users.noreply.github.com>
2021-04-08 21:58:39 +0100

    Commit Inicial

---
+++ TheMoviePlug/TheMoviePlug/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheMoviePlug.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
